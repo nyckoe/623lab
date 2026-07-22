f8e060f5087c01a0223310306f4c854d713c67b9
C. L. <53353883+nyckoe@users.noreply.github.com>
2026-07-21 21:16:11 -0400

    delete folder

--- CKare/apps/ckare-care/app/_layout.tsx
+++
@@ -1,8 +0,0 @@
-import { Stack } from "expo-router";
-import { StatusBar } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { CareDemoProvider } from "../src/CareDemoContext";
-
-export default function RootLayout() {
-  return <SafeAreaProvider><StatusBar barStyle="dark-content" /><CareDemoProvider><Stack screenOptions={{ headerShown: false }} /></CareDemoProvider></SafeAreaProvider>;
-}
--- CKare/packages/core/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./types.ts";
-export * from "./status.ts";
-export * from "./sample-data.ts";
